bccd9a7056cb54c7c2ee7f410fec2742b361e533
Seong Hyun Hwang <stathwang@users.noreply.github.com>
2018-01-06 18:17:12 -0500

    Create preprocess.R

---
+++ model/R/preprocess.R
@@ -0,0 +1,16 @@
+library(Matrix)
+library(plyr)
+library(data.table)
+library(ggplot2)
+library(pryr)
+
+f <- fread("data/douban_data_trunc.tsv")
+
+# stratified split into train and test
+set.seed(321)
+test <- rbindlist(llply(unique(f$user_id), function(x) {
+  temp <- f[user_id == x]
+  test_ind <- sample(seq_len(temp[,.N]), size = floor(0.25 * temp[,.N]))
+  return(temp[test_ind])
+}))
+train <- setdiff(f, test)
